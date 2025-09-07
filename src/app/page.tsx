e5b3138c7c4328a07cbfabc23a0123c401b8004b
ricejson <wjjbangbangbang@163.com>
2025-09-07 16:23:10 +0800

    初始化面试刷题平台前端代码

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import Image from "next/image";
+import { Button } from "antd";
 import styles from "./page.module.css";
 
 export default function Home() {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
+        <Button type="primary">测试按钮</Button>
         <Image
           className={styles.logo}
           src="/next.svg"
