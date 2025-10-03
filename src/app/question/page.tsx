714de0e44598515de05eb2a148bbacc9de38de71
ricejson <wjjbangbangbang@163.com>
2025-10-03 23:20:38 +0800

    开发题目列表大全页面

--- src/app/question/page.tsx
+++ src/app/question/page.tsx
@@ -1,8 +1,28 @@
+"use server";
+import "./index.css";
+import {listQuestionVoByPageUsingPost} from "@/api/questionController";
+import QuestionTable from "@/components/QuestionTable";
 
-export default function QuestionPage() {
+export default async function QuestionPage() {
+    let defaultQuestionList = [];
+    try {
+        const res = await listQuestionVoByPageUsingPost({
+            sortField: 'createTime',
+            sortOrder: 'descend',
+            pageSize: 200,
+        });
+        if (res && res.code === 0) {
+            defaultQuestionList = res.data?.records ?? [];
+            console.log(defaultQuestionList);
+        } else {
+            throw new Error('获取题目列表失败！' + res.message);
+        }
+    } catch (e) {
+        console.log(e)
+    }
     return (
-        <>
-            题目页
-        </>
+        <div id={"question-table"}>
+            <QuestionTable defaultQuestionList={defaultQuestionList}></QuestionTable>
+        </div>
     )
 }
---
+++ src/components/QuestionTable/index.tsx
@@ -0,0 +1,96 @@
+"use client";
+import './index.css';
+import React, {useEffect, useRef, useState} from "react";
+import {ActionType, ProColumns, ProTable} from "@ant-design/pro-components";
+import {listQuestionVoByPageUsingPost} from "@/api/questionController";
+import TagList from "@/components/TagList";
+
+interface Props {
+    defaultQuestionList: API.QuestionVO[]
+}
+
+const QuestionTable: React.FC = (props: Props) => {
+    const [questionList, setQuestionList] = useState<API.QuestionVO[]>([]);
+    // 是否第一次渲染（走默认）
+    const [isFirstLoad, setIsFirstLoad] = useState<boolean>(true);
+    const { defaultQuestionList } = props;
+    useEffect(() => {
+        if (defaultQuestionList && defaultQuestionList.length > 0) {
+            setQuestionList(defaultQuestionList);
+        }
+    }, [])
+    const columns: ProColumns<API.QuestionVO>[] = [
+        {
+            title: '名称',
+            dataIndex: 'title',
+            valueType: 'text',
+            ellipsis: true,
+        },
+        {
+            title: '标签',
+            dataIndex: 'tagList',
+            valueType: 'text',
+            render: (text, record, _, action) => {
+                // 解析json标签数据
+                return <TagList tagList={record.tagList ?? []}></TagList>
+            },
+        },
+    ];
+
+    const actionRef = useRef<ActionType>();
+    return (
+        <div id={"question-table"}>
+            <ProTable
+                columns={columns}
+                cardBordered
+                actionRef={actionRef}
+                dataSource={questionList}
+                request={async (params: API.QuestionQueryRequest, sort, filter) => {
+                    if (isFirstLoad && defaultQuestionList && defaultQuestionList.length > 0) {
+                        setIsFirstLoad(false);
+                        return;
+                    }
+                    const res = await listQuestionVoByPageUsingPost({
+                        ...params,
+                        ...sort,
+                        ...filter
+                    });
+                    if (res && res?.code === 0) {
+                        // 更新表格渲染数据
+                        setQuestionList(res.data?.records ?? []);
+                    }
+                }}
+                editable={{
+                    type: 'multiple',
+                }}
+                columnsState={{
+                    persistenceKey: 'pro-table-singe-demos',
+                    persistenceType: 'localStorage',
+                    defaultValue: {
+                        option: {fixed: 'right', disable: true},
+                    },
+                    onChange(value) {
+                        console.log('value: ', value);
+                    },
+                }}
+                rowKey="id"
+                search={{
+                    labelWidth: 'auto',
+                }}
+                options={{
+                    setting: {
+                        listsHeight: 400,
+                    },
+                }}
+                pagination={{
+                    pageSize: 20,
+                    onChange: (page) => console.log(page),
+                }}
+                dateFormatter="string"
+                headerTitle="题目列表"
+            />
+        </div>
+    )
+}
+
+export default QuestionTable;
