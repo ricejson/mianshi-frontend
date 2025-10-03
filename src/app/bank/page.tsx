297dd12b5fd8dedb5f35349cbf7ba4c07b51f2e8
ricejson <wjjbangbangbang@163.com>
2025-10-03 16:48:08 +0800

    开发题库大全页面

--- src/app/bank/page.tsx
+++ src/app/bank/page.tsx
@@ -1,7 +1,32 @@
-export default function BankPage() {
+"use server";
+
+import './index.css';
+import {listQuestionBankVoByPageUsingPost} from "@/api/questionBankController";
+import BankList from "@/components/BankList";
+import Title from "antd/es/typography/Title";
+
+export default async function BankPage() {
+    let bankList = [];
+    try {
+        const res = await listQuestionBankVoByPageUsingPost({
+            pageSize: 200,
+            sortOrder: 'desc',
+            sortField: 'createTime',
+        });
+        if (res && res.code === 0) {
+            bankList = res.data.records;
+        } else {
+            throw new Error('获取题库失败！' + res.message);
+        }
+    } catch (e) {
+
+    }
     return (
-        <>
-            题库页
-        </>
+        <div id={"bank-page"}>
+            <Title level={3}>
+                题库大全
+            </Title>
+            <BankList bankList={bankList}></BankList>
+        </div>
     )
 }
