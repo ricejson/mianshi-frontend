feab71cd07211aeacadcf003586216f8b8e737ad
ricejson <wjjbangbangbang@163.com>
2025-10-12 01:11:25 +0800

    修改后端接口地址

--- src/libs/request.ts
+++ src/libs/request.ts
@@ -5,7 +5,7 @@ import axios from "axios";
  * @author ricejson
  */
 const myAxios = axios.create({
-    baseURL: "https://mianshi-backend-107148-4-1326559147.sh.run.tcloudbase.com",
+    baseURL: "https://mianshi-backend-107148-4-1326559147.sh.run.tcloudbase.com:8101",
     timeout: 10000,
     withCredentials: true, // 携带cookie参数
 })
